c66af06098c7e3c99a661e6619ed8721d0971dce
James <james.veugelaers@outlook.co.nz>
2026-02-10 21:04:20 +1300

    Initial Commit

---
+++ Assets/Tests/BuildUploaderTests.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+public class BuildUploaderTests
+{
+    [Test]
+    public void SucceedTest()
+    {
+        Assert.AreEqual(3, 3);
+    }
+    
+    [Test]
+    public void FailTest()
+    {
+        Assert.AreNotEqual(3, 2);
+    }
+    
+    [UnityTest]
+    public IEnumerator SucceedUnityTest()
+    {
+        yield return null;
+        Assert.AreEqual(3, 3);
+    }
+}
